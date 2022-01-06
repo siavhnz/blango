d5c8e2e084304332d5a9e64ca5a5344c78073bef
mehdi hosseinzadeh <siavhnz@gmail.com>
2022-01-06 06:40:05 +0000

    Finish Introduction and Django Admingit add .

--- blango/settings.py
+++ blango/settings.py
@@ -46,9 +46,11 @@ class Dev(Configuration):
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
+        "debug_toolbar",
     ]
 
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -195,6 +197,9 @@ class Dev(Configuration):
       'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
       'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     ]
+
+    INTERNAL_IPS = ["192.168.10.93"]
+
 
 class Prod(Dev):
     Debug = values.BooleanValue(False)
--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ def index(request):
     from django.http import HttpResponse
     logger.debug("Index function is called!")
     #return HttpResponse(str(request.user).encode("ascii"))
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
   
@@ -43,3 +43,7 @@ def post_detail(request, slug):
     return render(
         request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
     )
+
+def get_ip(request):
+  from django.http import HttpResponse
+  return HttpResponse(request.META['REMOTE_ADDR'])
