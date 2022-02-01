de857df545a24cc33f861ea8f6febfc59d2993df
mehdi hosseinzadeh <siavhnz@gmail.com>
2022-02-01 06:40:09 +0000

    Finish caching

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics, viewsets
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
+from rest_framework.exceptions import PermissionDenied
 
 from blog.api.serializers import(
     PostSerializer,
@@ -27,6 +31,14 @@ class TagViewSet(viewsets.ModelViewSet):
         )
         return Response(post_serializer.data)
 
+    @method_decorator(cache_page(300))
+    def list(self, *args, **kwargs):
+      return super(TagViewSet, self).list(*args, **kwargs)
+
+    @method_decorator(cache_page(300))
+    def retrieve(self, *args, **kwargs):
+      return super(TagViewSet, self).retrieve(*args, **kwargs)
+
 class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
@@ -36,6 +48,20 @@ class PostViewSet(viewsets.ModelViewSet):
             return PostSerializer
         
         return PostDetailSerializer
+
+    @method_decorator(cache_page(300))
+    @method_decorator(vary_on_headers("Authorization", "cookie"))
+    @action(methods=["get"], detail=False, name="Posts by the logged in user")
+    def mine(self, request):
+      if request.user.is_anonymous:
+        raise PermissionDenied("You must be logged in to see which Posts are yours")
+      posts = self.get_queryset().filter(author=request.user)
+      serializer = PostSerializer(posts, many=True, context={"request": request})
+      return Response(serializer.data)
+
+    @method_decorator(cache_page(120))
+    def list(self, *args, **kwargs):
+      return super(PostViewSet, self).list(*args, **kwargs)
 
 class PostList(generics.ListCreateAPIView):
     #authentication_classes = [SessionAuthentication]
@@ -52,3 +78,7 @@ class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @method_decorator(cache_page(300))
+    def get(self, *args, **kwargs):
+      return super(UserDetail, self).get(*args, **kwargs)
