703b35fdce72353c473eab1cac16d5a375029cc1
mehdi hosseinzadeh <siavhnz@gmail.com>
2022-03-02 14:00:26 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
 urlpatterns = [
   #path("posts/", PostList.as_view(), name="api_post_list"),
   #path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail",),
-  path("users/<str:email>", UserDetail.as_view(), name="api_user_detail")
+  path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
@@ -55,5 +55,10 @@ urlpatterns += [
     ),
 
     path("", include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
 ]
 
